ab9102a1ad7454ac8e4291bba7032ae5164e9e81
labstraction <labstraction@protonmail.com>
2024-04-11 22:08:54 +0000

    scan tokens

--- src/scanner2/mod.rs
+++ src/scanner2/mod.rs
@@ -46,43 +46,80 @@ pub enum TokenType {
     Eof,
 }
 
-pub enum Literal{
-
+#[derive(Debug)]
+pub enum Literal {
+    Empty,
 }
 
 #[derive(Debug)]
 pub struct Token {
-  pub t_type: TokenType,
-  pub lexeme: String,
-  pub literal: Literal,
-  pub line: i32,
+    pub t_type: TokenType,
+    pub lexeme: String,
+    pub literal: Literal,
+    pub line: usize,
 }
 
 impl Token {
-
-    pub fn new(t_type: TokenType, lexeme: String, literal: Literal, line: i32) -> Token{
-        Token{t_type, lexeme, literal, line}
+    pub fn new(t_type: TokenType, lexeme: String, literal: Literal, line: usize) -> Token {
+        Token {
+            t_type,
+            lexeme,
+            literal,
+            line,
+        }
     }
 
-    pub fn to_string(&self) -> String{
-        format!("type: {}, lexeme: {}, literal:{}", self.t_type, self.lexeme, self.literal)
+    pub fn to_string(&self) -> String {
+        format!(
+            "type: {:#?}, lexeme: {}, literal:{:#?}",
+            self.t_type, self.lexeme, self.literal
+        )
     }
 }
 
 #[derive(Debug)]
 pub struct Scanner {
-  pub source: String,
-  pub tokens: Vec<Token>
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
 }
 
-
 impl Scanner {
-
-    pub fn new(source: String) -> Scanner{
-        Scanner(source, Vec::new());
+    pub fn new(source: String) -> Scanner {
+        Scanner {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1
+        }
     }
 
-    pub fn to_string(&self) -> String{
-        format!("type: {}, lexeme: {}, literal:{}", self.t_type, self.lexeme, self.literal)
+    pub fn scan_tokens(&self) {
+
+        while !self.is_at_end() {
+            // We are at the beginning of the next lexeme.
+            self.start = self.current;
+            scanToken();
+        }
+
+        self.tokens.push(Token::new(
+            TokenType::Eof,
+            String::from(""),
+            Literal::Empty,
+            self.line,
+        ));
+    }
+
+    fn is_at_end(&self) -> bool{
+        return self.current >= self.source.len();
+    }
+
+    fn advance(&self) -> char{
+        let c = self.source.chars().nth(self.current).unwrap_or(' ');
+        self.current+=1;
+        c
     }
 }
